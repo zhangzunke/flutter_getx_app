898e85044bc4e5a9351726ad767cc70fedf10768
Michael <zhangzunke@126.com>
2025-09-14 22:20:11 +0800

    add content page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getx_app/content_page.dart';
+import 'package:flutter_getx_app/my_detail_page.dart';
 import 'package:flutter_getx_app/my_home_page.dart';
 import 'package:get/get.dart';
 
@@ -16,7 +18,11 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
       initialRoute: '/',
-      getPages: [GetPage(name: '/', page: () => const MyHomePage())],
+      getPages: [
+        GetPage(name: '/', page: () => const MyHomePage()),
+        GetPage(name: '/content', page: () => const ContentPage()),
+        GetPage(name: '/detail', page: () => const DetailPage()),
+      ],
     );
   }
 }
