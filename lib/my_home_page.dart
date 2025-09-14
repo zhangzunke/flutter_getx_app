b1fa084f9f879c59569a5c9643172007919903e4
Michael <zhangzunke@126.com>
2025-09-14 20:30:29 +0800

    init project

---
+++ lib/my_home_page.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF69c5df),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            height: 700,
+            child: Container(
+              height: 700,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("images/background.jpg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 20,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Pick Your Favourite',
+                  style: TextStyle(
+                    fontSize: 32,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                Text(
+                  'Contests',
+                  style: TextStyle(
+                    fontSize: 32,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(height: 40),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width - 25,
+                  child: Text(
+                    'We make great design work happen with our great community designer',
+                    style: TextStyle(color: Colors.white60, fontSize: 16),
+                  ),
+                ),
+                SizedBox(height: 40),
+                Container(
+                  width: 200,
+                  height: 70,
+                  decoration: BoxDecoration(
+                    color: Color(0xFffbc33e),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      elevation: 0,
+                      backgroundColor: Color(0xFFfbc33e),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    onPressed: () => Container(),
+                    child: Text(
+                      'Get Started',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 70),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
